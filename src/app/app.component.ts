d111bbf338ae714a5101434931b051c5dc989ab4
Isahir Zavala Miranda <isahirzavala@hotmail.com>
2024-01-20 01:30:00 -0600

    drawing functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,116 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ButtonComponent } from "./components/button/button.component";
 import { ToastComponent } from "./components/toast/toast.component";
 import { ToastInfo } from './interfaces/toastInfo';
 import { ToastService } from './services/toast.service';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CanvaTools } from './interfaces/tools';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [CommonModule, RouterOutlet, ButtonComponent, ToastComponent]
+  imports: [CommonModule, RouterOutlet, ButtonComponent, ToastComponent, ReactiveFormsModule]
 })
 export class AppComponent {
-  constructor(public toastService: ToastService) {
+  private isPainting = false;
+  startX = 0;
+  startY = 0;
 
+  canvatools!: FormGroup;
+  constructor(private formBuilder: FormBuilder, public toastService: ToastService, private renderer: Renderer2) {
+
+    this.createContactForm();
+  }
+
+  createContactForm() {
+    this.canvatools = this.formBuilder.group({
+      colorcode: '#00000',
+      width: 2
+    });
   }
 
   ngOnInit(): void {
-    const canvas = document.getElementById('drawing-canva') as HTMLCanvasElement;
-    if (canvas != null) {
-    }
-    const strokeColor = document.getElementById('color') as HTMLInputElement
 
-    if (strokeColor != null) {
-      console.log(strokeColor.value)
-    }
 
   }
 
+  ngAfterViewInit() {
+
+    const canvas = document.getElementById('drawing-canva') as HTMLCanvasElement;
+    const ctx = canvas.getContext("2d");
+    if (ctx == null) {
+      return;
+    }
+    if (canvas == null) {
+      return;
+    }
+
+    const canvasOffsetX = canvas.offsetLeft;
+    const canvasOffsetY = canvas.offsetTop;
+    
+    // Set the background color
+    ctx.fillStyle = "#fff";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    // //draw something
+    // ctx.fillStyle = "#000000";
+    // ctx.fillRect(0, 0, 150, 75);
+
+    canvas.addEventListener('mousedown', (e) => {
+      this.isPainting = true;
+      this.startX = e.clientX
+      this.startY = e.clientY;
+    });
+    canvas.addEventListener('mouseup', (e) => {
+      this.isPainting = false;
+      ctx.stroke();
+      ctx.beginPath();
+    });
+
+    canvas.addEventListener('mousemove', (event) => {
+      const canvas = document.getElementById('drawing-canva') as HTMLCanvasElement;
+      const ctx = canvas.getContext("2d");
+      if (ctx == null) {
+        return;
+      }
+      if (canvas == null) {
+        return;
+      }
+      if (!this.isPainting) {
+        return;
+      }
+
+      if (ctx == null) return;
+
+      const formData = this.canvatools.value;
+
+      ctx.lineWidth = formData['width'];
+      ctx.strokeStyle = formData['colorcode']
+      ctx.lineCap = 'round';
+
+      const point = {
+        x: event.clientX - canvasOffsetX+12,
+        y: event.clientY  - canvasOffsetY+12,
+      };
+
+      ctx.lineTo(point.x, point.y);
+      ctx.stroke();
+    });
+  }
+
   saveImg(): void {
+    const canvas = document.getElementById('drawing-canva') as HTMLCanvasElement;
+    if (canvas == null) return;
+
+    const link = document.createElement('a');
+    link.download = 'download.png';
+    link.href = canvas.toDataURL();
+    //link.click();
+
     const toastInfo: ToastInfo = {
       title: 'Image saved!',
       type: ''
@@ -38,5 +118,21 @@ export class AppComponent {
     this.toastService.add(toastInfo);
   }
 
+  clean(){
+    
+    const canvas = document.getElementById('drawing-canva') as HTMLCanvasElement;
+    const ctx = canvas.getContext("2d");
+    if (ctx == null) {
+      return;
+    }
+    if (canvas == null) {
+      return;
+    }
+    
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    ctx.fillStyle = "#fff";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }
 }
 
---
+++ src/app/interfaces/tools.ts
@@ -0,0 +1,4 @@
+export interface CanvaTools{
+    colorCode:string,
+    lineWidth:number
+}
